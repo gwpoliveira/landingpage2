8ce9e62718754f7cd8b981e1944d4f7ca3680d39
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-10-18 13:32:59 -0300

    atualizando email não obrigatório

--- leads/models.py
+++ leads/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     phone = models.CharField(max_length=15)
     course = models.CharField(max_length=100)
 
     CONSULTOR_CHOICES = [
         ('Marisa Rodrigues', 'Marisa Rodrigues'),
         ('Jacob Ambrósio', 'Jacob Ambrósio'),
-        ('Vitor Leal', 'Vitor Leal'),
+        ('Vito Leal', 'Vito Leal'),
         ('Larissa Lari', 'Larissa Lari'),
         ('Thiago Gomes', 'Thiago Gomes'),
     ]
